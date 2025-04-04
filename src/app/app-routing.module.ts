ccedeeda20fdcf29b4f7e67f3a1e0b54e552031b
ybarrial <yefferson.barrial@avivel.com.pe>
2025-04-04 16:00:28 -0500

    add  app-routing module & app module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+  },
+  {
+    path: 'home',
+    component: LayoutComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ]
+})
+export class AppModule { }
